1424cd2b31f2daa03f55309b800722f3cb939761
Tomasz <lis109@wp.pl>
2022-03-29 20:01:44 +0200

    Exeption class

--- src/main/java/com/example/restauracja/web/EmployeeRestControlerAdvice.java
+++ src/main/java/com/example/restauracja/web/EmployeeRestControlerAdvice.java
@@ -1,8 +1,18 @@
 package com.example.restauracja.web;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class EmployeeRestControlerAdvice {
 
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(IndexOutOfBoundsException.class)
+    public String indexExeption(IndexOutOfBoundsException ie) {
+        return "Index out of bounds exeption" + ie.getMessage();
+    }
 }
